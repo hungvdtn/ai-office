b277dee71efd48cd628d64d3760cf4ba4938f2c1
thukyth <hungvdtnai@gmail.com>
2026-05-17 17:10:32 +0700

    Add files via upload

--- src/components/QRCodeStudio.tsx
+++ src/components/QRCodeStudio.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function QRCodeStudio() {
   return (
-    <div className="w-full h-full bg-[#f4f7f6] overflow-hidden flex flex-col">
+    <div className="w-full h-[calc(100vh-70px)] bg-[#f4f7f6] overflow-hidden">
       <iframe
         src="/qrcode/index.html"
-        className="w-full flex-1 border-none"
+        className="w-full h-full border-none m-0 p-0 block"
         title="Công cụ tạo mã QR"
         sandbox="allow-scripts allow-same-origin allow-downloads"
       />
